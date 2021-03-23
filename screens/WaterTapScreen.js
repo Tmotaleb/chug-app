5fde3c1523340ff697363f3b28bfab90cf7b2b67
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-03-23 16:07:44 -0400

    corganized components

---
+++ screens/WaterTapScreen.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View , SafeAreaView} from 'react-native';
+import styles from './components/style';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <TouchableOpacity>
+
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
