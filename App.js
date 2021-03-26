7f1fa60ab8fd5163f81c7a203f30d36a0f71494a
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-03-26 16:17:51 -0400

    added font files

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomePage from './screens/HomePage';
@@ -14,7 +14,6 @@ export default function App() {
       <AppStack.Navigator
         headerMode='none'
       >
-
         <AppStack.Screen name='Onboarding' component={OnBoarding}/>
         {/* <AppStack.screen /> */}
       </AppStack.Navigator>
