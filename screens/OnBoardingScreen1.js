ee12839274825ad90f65c8cea3cf21e3b4550094
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2021-06-09 16:41:26 -0400

    added onboarding screen 1

--- screens/OnBoardingScreen1.js
+++ screens/OnBoardingScreen1.js
@@ -20,14 +20,27 @@ import {intakeCSS, personalCSS, picker, personalIntake} from '../components/styl
 const OnBoardingScreen1 = ({navigation}) => {
 
 
+
   return (
     <View style={styles.container}>
 
-      <View style={styles.box1}>
-      </View>
-        <View style={styles.box2}>
-
+      <ImageBackground
+        source={require("../assets/images/girl-drink2.jpeg")}
+        style={styles.box1}
+        // imageStyle={{resizeMode: 'contain'}}
+      >
+        <View style={styles.titleBox}>
+          <Text style={styles.title}>HYDRATE</Text>
         </View>
+
+        {/* <View style={styles.subtitleBox}>
+          <Text style={styles.subtitle}>The quest for hydration is very simple.</Text>
+          <Text style={styles.subtitle}>Give your body the balance it needs to maintain itself.</Text>
+        </View> */}
+      </ImageBackground>
+
+
+      <View style={styles.box2}></View>
 
       <View style={styles.bottomBar}>
         <TouchableOpacity style={{alignSelf: 'flex-end', padding: 20}} onPress={()=> navigation.navigate('OnBoardingScreen2')}>
@@ -43,16 +56,58 @@ const styles = StyleSheet.create ({
   container: {
     flex: 1,
     // justifyContent: 'center',
-    // alignItems: 'center'
+    // alignItems: 'center',
+    // flexDirection: "column"
   },
   box1: {
     // flex:1,
+    // height: 800,
+    // // backgroundColor: 'pink',
+    resizeMode: "cover",
+    // justifyContent: "center",
     height: 800,
-    backgroundColor: 'pink'
+    width: '100%'
   },
  bottomBar: {
    height: 60
- }
+ },
+
+  titleBox: {
+  position: 'absolute',
+  top: 0,  //80
+  left: 15,
+  right: 380,
+  bottom: 195,
+  // backgroundColor: 'pink',
+  justifyContent: "center",
+
+  alignItems: 'center'
+  // // color: 'white',
+  // // fontSize: 25
+  },
+  title: {
+    color: 'white',
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Kailasa',
+    // transform: [{ rotate: '-90deg'}]
+  },
+  subtitleBox : {
+    position: 'absolute',
+    top: 160,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  subtitle : {
+    color: 'white',
+    fontSize: 15,
+    fontFamily: 'Kailasa',
+    textAlign: 'justify',
+    marginHorizontal: 20
+  },
 })
 
 
